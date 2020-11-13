aeb63db35638160d405d4b18f530aa5b14b0e548
Gualtiero Frigerio <gualtiero.frigerio@gmail.com>
2020-11-13 16:16:19 +0100

    added OCRViewController

--- OCRTest/ViewController.swift
+++ OCRTest/ViewController.swift
@@ -11,7 +11,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //performOCROnAssets()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let ocrVC = OCRViewController()
+        self.present(ocrVC, animated: true, completion: nil)
+    }
+    
+    func performOCROnAssets() {
         let images = ["ocrtest", "lorem", "loremwikipedia"]
         for imageName in images {
             if let image = UIImage(named: imageName) {
