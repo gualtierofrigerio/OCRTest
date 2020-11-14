fe4fd7fbc95f0661c26b085aa803113de513dbb8
Gualtiero Frigerio <gualtiero.frigerio@gmail.com>
2020-11-14 15:37:48 +0100

    added image orientation

--- OCRTest/OCRHelper.swift
+++ OCRTest/OCRHelper.swift
@@ -16,6 +16,7 @@ typealias OCRHelperCallback = (Bool, [String]?) -> Void
 /// once the OCR is done
 fileprivate struct OCRHelperRequest {
     var image:CGImage
+    var orientation:CGImagePropertyOrientation?
     var callback:OCRHelperCallback
 }
 
@@ -29,21 +30,25 @@ class OCRHelper {
     ///   - image: The UIImage to scan for text
     ///   - callback: the callback with a bool parameter indicating success
     ///                 and an optional array of string recognized in the image
-    func getTextFromImage(_ image:UIImage, callback:@escaping OCRHelperCallback) {
+    func getTextFromImage(_ image:UIImage,
+                          callback:@escaping OCRHelperCallback) {
         guard let cgImage = image.cgImage else {
             callback(false, nil)
             return
         }
-        addRequest(withImage: cgImage, callback: callback)
+        addRequest(withImage: cgImage, orientation:nil, callback: callback)
     }
     
     /// Get an array of strings from a CGImage
     /// - Parameters:
     ///   - image: The CGImage to scan for text
+    ///   - orientation: The adjusted orientation of the image
     ///   - callback: the callback with a bool parameter indicating success
     ///                 and an optional array of string recognized in the image
-    func getTextFromImage(_ image:CGImage, callback:@escaping OCRHelperCallback) {
-        addRequest(withImage: image, callback: callback)
+    func getTextFromImage(_ image:CGImage,
+                          orientation:CGImagePropertyOrientation?,
+                          callback:@escaping OCRHelperCallback) {
+        addRequest(withImage: image, orientation:orientation, callback: callback)
     }
     
     // MARK: - Private
@@ -51,16 +56,33 @@ class OCRHelper {
     private var pendingOCRRequests:[OCRHelperRequest] = []
     private var useFastRecognition = false
     
-    private func addRequest(withImage image:CGImage, callback:@escaping OCRHelperCallback) {
-        let request = OCRHelperRequest(image: image, callback: callback)
+    /// Add a request for OCR
+    /// - Parameters:
+    ///   - image: The CGImage to scan for text
+    ///   - orientation: the CGImage adjusted orientation
+    ///   - callback: callback with the recognized text
+    private func addRequest(withImage image:CGImage,
+                            orientation:CGImagePropertyOrientation?,
+                            callback:@escaping OCRHelperCallback) {
+        let request = OCRHelperRequest(image: image, orientation:orientation, callback: callback)
         pendingOCRRequests.append(request)
         if pendingOCRRequests.count == 1 {
             processOCRRequest(request)
         }
     }
     
+    /// Process the next request in queue
+    /// - Parameter request: The OCRHelperRequest to process
     private func processOCRRequest(_ request:OCRHelperRequest) {
-        let requestHandler = VNImageRequestHandler(cgImage: request.image)
+        var requestHandler:VNImageRequestHandler
+        if let orientation = request.orientation {
+            requestHandler = VNImageRequestHandler(cgImage: request.image,
+                                                   orientation: orientation,
+                                                   options: [:])
+        }
+        else {
+            requestHandler = VNImageRequestHandler(cgImage: request.image)
+        }
         let visionRequest = VNRecognizeTextRequest(completionHandler: recognizeTextHandler)
         visionRequest.recognitionLevel = useFastRecognition ? .fast : .accurate
         do {
@@ -71,6 +93,10 @@ class OCRHelper {
         }
     }
     
+    /// The handler called by Vision when an image has been processed
+    /// - Parameters:
+    ///   - request: the VNRequest processed
+    ///   - error: optional Error
     private func recognizeTextHandler(request: VNRequest, error: Error?) {
         guard let observations = request.results as? [VNRecognizedTextObservation] else {
             currentRequestProcessed(strings: nil)
@@ -82,6 +108,8 @@ class OCRHelper {
         currentRequestProcessed(strings: recognizedStrings)
     }
     
+    /// Called when the current request has been processed
+    /// - Parameter strings: Optional array with recognized text
     private func currentRequestProcessed(strings:[String]?) {
         guard let request = pendingOCRRequests.first else {
             return
