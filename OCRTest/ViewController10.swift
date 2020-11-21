fe3562859ff8187f625eea3b1770e7bbfc512551
Gualtiero Frigerio <gualtiero.frigerio@gmail.com>
2020-11-21 18:00:43 +0100

    added ViewController iOS 10 compatible

---
+++ OCRTest/ViewController10.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController11.swift
+//  OCRTest
+//
+//  Created by Gualtiero Frigerio on 20/11/2020.
+//
+
+import UIKit
+import GFLiveScanner
+
+class ViewController10: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func barcodeTap(_ sender: Any) {
+        let liveScanVC = GFLiveScannerViewController(withDelegate: self, options: nil)
+        self.present(liveScanVC, animated: true, completion: nil)
+        liveScanVC.startScanning(mode: .barcode)
+    }
+    
+    
+    @IBAction func ocrTap(_ sender: Any) {
+        if #available(iOS 13.0, *) {
+            if let vc = storyboard?.instantiateViewController(withIdentifier: "ViewController") as? ViewController {
+                present(vc, animated: true, completion: nil)
+            }
+        } else {
+            print("available only on iOS 13")
+        }
+    }
+}
+
+extension ViewController10:GFLiveScannerDelegate {
+    func capturedStrings(strings: [String]) {
+        print("captured barcodes -> \(strings)")
+    }
+    
+    func liveCaptureEnded(withError: Error?) {
+        print("liveCaptureEnded")
+    }
+}
