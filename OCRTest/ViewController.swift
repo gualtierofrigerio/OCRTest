b2705ff9f4cf2639b8c57476424d00881dda3126
Gualtiero Frigerio <gualtiero.frigerio@gmail.com>
2020-11-21 18:00:06 +0100

    added UIImagePicker

--- OCRTest/ViewController.swift
+++ OCRTest/ViewController.swift
@@ -7,8 +7,10 @@
 
 import Combine
 import UIKit
+import GFLiveScanner
 
-class ViewController: UIViewController {
+@available(iOS 13.0, *)
+class ViewController: UIViewController, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,7 @@ class ViewController: UIViewController {
         super.viewDidAppear(animated)
     }
     
+    @available(iOS 13.0, *)
     func performOCROnAssets() {
         let images = ["ocrtest", "lorem", "loremwikipedia"]
         for imageName in images {
@@ -29,50 +32,73 @@ class ViewController: UIViewController {
         }
     }
     
+    @available(iOS 13.0, *)
     func performOCR(onImage image:UIImage) {
-        let helper = OCRHelper(fastRecognition: false)
+        let helper = GFOcrHelper(fastRecognition: false)
         helper.getTextFromImage(image) { success, strings in
             print("success \(success) strings \(String(describing: strings))")
         }
     }
     
     // MARK: - Private
-    private var ocrViewController = OCRViewController()
     
     @IBAction private func tapOnLiveScan(_ sender:Any) {
-        self.present(ocrViewController, animated: true, completion: nil)
-        let publisher = ocrViewController.startLiveScan()
-        cancellable = publisher.sink { strings in
-            print("recognized strings: \(strings)")
-        }
+        let liveScanVC = GFLiveScannerViewController(withDelegate: self, options: nil)
+        self.present(liveScanVC, animated: true, completion: nil)
+        liveScanVC.startScanning(mode: .ocr)
     }
     
     @IBAction private func tapOnCamera(_ sender:Any) {
-        self.present(ocrViewController, animated: true, completion: nil)
-        ocrViewController.getTextFromCamera { (succes, strings) in
-            if let strings = strings {
-                print("recognized strings from camera: \(strings)")
-            }
-            else {
-                print("getTextFromCamera return false")
-            }
-            self.ocrViewController.dismiss(animated: true, completion: nil)
-        }
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = self
+        present(picker, animated: true)
     }
     
     @IBAction private func tapOnLibrary(_ sender:Any) {
-        self.present(ocrViewController, animated: true, completion: nil)
-        ocrViewController.getTextFromLibrary { (succes, strings) in
-            if let strings = strings {
-                print("recognized strings from camera: \(strings)")
-            }
-            else {
-                print("getTextFromCamera return false")
-            }
-            self.ocrViewController.dismiss(animated: true, completion: nil)
-        }
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        present(picker, animated: true)
     }
 
     private var cancellable:AnyCancellable?
+    private var ocrHelper = GFOcrHelper(fastRecognition: false)
 }
 
+// MARK: - UIImagePicker delegate
+
+@available(iOS 13.0, *)
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[.originalImage] as? UIImage else {
+            print("No image found")
+            return
+        }
+        ocrHelper.useFastRecognition = false
+        ocrHelper.getTextFromImage(image) { success, strings in
+            if let strings = strings {
+                print(strings)
+            }
+            else {
+                print("no strings recognized")
+            }
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+extension ViewController: GFLiveScannerDelegate {
+    func capturedStrings(strings: [String]) {
+        print("captured -> \(strings)")
+    }
+    
+    func liveCaptureEnded(withError error:Error?) {
+        print("live capture ended")
+    }
+}
